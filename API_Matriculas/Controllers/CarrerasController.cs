809cd1910b50a1cdba71554b8041d0ec7f03825a
Jose-Picado-Zamora <joseandrespicadozamora9@gmail.com>
2024-06-15 09:57:54 -0600

    cambios

---
+++ API_Matriculas/Controllers/CarrerasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Matriculas.Controllers
+{
+    public class CarrerasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ API_Matriculas/Controllers/CursosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Matriculas.Controllers
+{
+    public class CursosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
