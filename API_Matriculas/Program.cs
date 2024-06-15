aefb8e9406855205ef59ef361945e2fc13ef0844
Alita05 <agarfon1@gmail.com>
2024-06-15 10:48:47 -0600

    db

--- API_Matriculas/Program.cs
+++ API_Matriculas/Program.cs
@@ -1,8 +1,35 @@
+using Services;
+using Services.Niveles;
+using Services.Matriculas;
+using Services.Estudiantes;
+using Services.Cursos;
+using Services.Carreras;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
+builder.Services.AddScoped<ISvCarrera, SvCarrera>();
+builder.Services.AddScoped<IsVCursos, SvCursos>();
+builder.Services.AddScoped<IsVEstudiantes, SvEstudiantes>();
+builder.Services.AddScoped<IsVMatriculas, SvMatriculas>();
+builder.Services.AddScoped<IsVNiveles, SvMatriculas>();
 
-builder.Services.AddControllers();
+//
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(x =>
+ x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Policy1", builder =>
+    {
+        builder.WithOrigins("https://localhost:5173")
+            .WithMethods("GET", "POST")
+            .WithHeaders("Content-Type");
+    });
+
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Services/MyDbContext/MyDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Services.MyDbContext
+{
+    public class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("MyDatabase");
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<Carrera> Carreras { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+        public DbSet<Curso> Categories { get; set; }
+        public DbSet<Nivel> Niveles { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Curso>()
+                .HasOne(nivel => nivel.Nivel);
+
+            modelBuilder.Entity<Curso>()
+                .HasMany(estudiantes => estudiantes.ListaEstudiantes)
+                .WithOne(curso => curso.Curso);
+
+            modelBuilder.Entity<Matricula>()
+                .HasOne(matricula => matricula.Estudiante);
+
+            modelBuilder.Entity<Matricula>()
+                .HasOne(matricula => matricula.Curso);
+
+            modelBuilder.Entity<Estudiante>()
+                .HasOne(curso => curso.Curso)
+                .WithMany(estudiantes => estudiantes.ListaEstudiantes);
+
+            modelBuilder.Entity<Carrera>()
+                .HasMany(cursos => cursos.ListaCursos);
+        }
+    }
+}
